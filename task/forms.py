7e35518e9ee64cd11568e9cedfa08a6661534f0a
OlenaLapenko <lapenko.e.i@gmail.com>
2022-08-11 16:30:19 +0300

    version 2

--- task/forms.py
+++ task/forms.py
@@ -1,11 +1,16 @@
-from datetime import datetime, date
-
 from django import forms
 from django.forms import DateInput
+from django.core.exceptions import ValidationError
 
 
 class InputForm(forms.Form):
     email = forms.EmailField()
     text = forms.CharField(max_length=200, help_text="Enter the reminder text")
     sending_time = forms.DateTimeField(required=False, widget=DateInput(attrs={'type': 'datetime-local'}),
-                                       initial=datetime.now(), localize=True)
+                                       localize=True)
+
+    def clean_data(self):
+        email = self.cleaned_data['email']
+        if "fred@example.com" not in email:
+            raise ValidationError("You have forgotten about Fred!")
+        return email
